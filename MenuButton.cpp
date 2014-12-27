66b2f02b9730d82facee8717aec6a03aea243fd5
Trevor M <metatrevor@gmail.com>
2014-12-27 20:46:54 +0000

    Merge repo

---
+++ MenuButton.cpp
@@ -0,0 +1,5 @@
+#include "MenuButton.h"
+
+MenuButton::MenuButton()
+{
+}
---
+++ MenuButton.h
@@ -0,0 +1,10 @@
+#ifndef MENUBUTTON_H
+#define MENUBUTTON_H
+
+class MenuButton
+{
+public:
+    MenuButton();
+};
+
+#endif // MENUBUTTON_H
