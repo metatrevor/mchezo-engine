33ef5172f243d3733e3db1134e1f0060bfb8e5ce
Trevor Mutune <metatrevor@gmail.com>
2015-03-03 14:52:05 +0300

    Sound interface params

--- src/Defines.h
+++ src/Defines.h
@@ -1,7 +1,15 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+#include <SDL2/SDL_mixer.h>
+
 const int FPS = 60;
 const int FRAMEDELTA = 1000.0f / FPS;
+
+#define SOUND_FREQUENCY 22050
+#define SOUND_FORMAT AUDIO_S16
+#define SOUND_CHANNELS 2
+#define SOUND_CHUNKSIZE 4096
+
 
 #endif // DEFINES_H
