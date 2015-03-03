7a6185cb0c9034bc3f43bce70e32f952179924ab
Trevor Mutune <metatrevor@gmail.com>
2015-03-03 14:55:06 +0300

    SoundManager sound fx

---
+++ src/SoundManager.cpp
@@ -0,0 +1,48 @@
+#include "SoundManager.h"
+#include "Log.h"
+
+SoundManager::SoundManager()
+{
+    Mix_OpenAudio(SOUND_FREQUENCY, SOUND_FORMAT, SOUND_CHANNELS, SOUND_CHUNKSIZE);
+}
+
+SoundManager &SoundManager::instance()
+{
+    static SoundManager m_instance;
+    return m_instance;
+}
+
+bool SoundManager::loadSound(std::string keyName, std::string fileName, audio_type type) {
+    if(type == SOUND_SFX)
+    {
+        Mix_Chunk *chunk = Mix_LoadWAV(fileName.c_str());
+        if(chunk == 0) {
+            Log::Error("Failed to load SFX :  " + fileName);
+            Log::Error(Mix_GetError());
+            return false;
+        }
+        else {
+            m_soundbuffers[keyName] = chunk;
+            return true;
+        }
+    }
+
+    else if(type == SOUND_MUSIC)
+    {
+
+    }
+
+    else {
+        Log::Error("Failed to load invalid file type " + fileName);
+        return false;
+    }
+
+}
+
+void SoundManager::playSound(std::string keyName, bool loop) {
+
+}
+
+void SoundManager::playMusic(std::string keyName, bool loop) {
+
+}
---
+++ src/SoundManager.h
@@ -0,0 +1,38 @@
+#ifndef _SOUNDMANAGER_H_
+#define _SOUNDMANAGER_H_
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <iostream>
+#include <map>
+#include "Defines.h"
+
+
+enum audio_type
+{
+    SOUND_MUSIC = 0,
+    SOUND_SFX = 1
+};
+
+class SoundManager
+{
+public:
+
+    static SoundManager &instance();
+    bool loadSound(std::string keyName, std::string fileName, audio_type type);
+
+    void playSound(std::string keyName, bool loop);
+    void playMusic(std::string keyName, bool loop);
+
+
+private:
+    SoundManager();
+    ~SoundManager(){}
+    SoundManager(const SoundManager&){}
+    SoundManager & operator=(const SoundManager&){}
+
+    std::map<std::string, Mix_Chunk*> m_soundbuffers;
+    std::map<std::string, Mix_Music*> m_musicbuffers;
+};
+
+#endif //SOUNDMANAGER_H_
